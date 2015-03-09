75c8ffb6c9c35e3db3368bbd70ecc2c7a1836199
Christophe Hamerling <christophe.hamerling@gmail.com>
2015-03-09 12:22:23 +0100

    OM-15 Use esn-config to get the sender email adress

--- test/lib/module.js
+++ test/lib/module.js
@@ -4,9 +4,23 @@ var expect = require('chai').expect;
 
 describe('The emailInvitation lib', function() {
 
+  var mail = {
+    mail: {
+      noreply: 'noreply@hubl.in'
+    },
+    transport: {
+      module: 'nodemailer-unknownmodule',
+      type: 'bar',
+      config: {
+      }
+    }
+  };
+
   beforeEach(function(done) {
     var self = this;
+
     this.depStore = {
+      logger: require('../fixtures/logger-noop')(),
       mailer: {
         setTemplatesDir: function() {}
       },
@@ -14,6 +28,13 @@ describe('The emailInvitation lib', function() {
         return {
           emailInvitation: {
             templatesDir: '/tmp/pipo'
+          }
+        };
+      },
+      'esn-config': function() {
+        return {
+          get: function(callback) {
+            callback(null, mail);
           }
         };
       }
@@ -40,17 +61,48 @@ describe('The emailInvitation lib', function() {
   });
 
   describe('exposed sendInvitation function', function() {
-    it('should throw if from has no id property', function(done) {
-      this.emailInvitation.sendInvitation({}, {objectType: 'user', id: 'robert@pipo.com'}, {url: 'anURL'}, function(err) {
-        expect(err).to.exist;
-        done();
+
+    it('should send back error if error while getting sender address', function(done) {
+      var from = {objectType: 'email', id: 'yo@hubl.in'};
+      var to = {objectType: 'email', id: 'lo@hubl.in'};
+
+      this.depStore['esn-config'] = function() {
+        return {
+          get: function(callback) {
+            return callback(new Error());
+          }
+        };
+      };
+
+      require('../../lib/module')(this.dependencies, function(err, lib) {
+        expect(err).to.not.exist;
+        lib.sendInvitation(from, to, {url: 'anURL'}, function(err) {
+          expect(err).to.exist;
+          expect(err.message).to.match(/Error while getting email sender/);
+          done();
+        });
       });
     });
 
-    it('should throw if from has not an email as id property', function(done) {
-      this.emailInvitation.sendInvitation({objectType: 'user', id: 'notanemail'}, {objectType: 'user', id: 'robert@pipo.com'}, {url: 'anURL'}, function(err) {
-        expect(err).to.exist;
-        done();
+    it('should send back error if can not get sender address', function(done) {
+      var from = {objectType: 'email', id: 'yo@hubl.in'};
+      var to = {objectType: 'email', id: 'lo@hubl.in'};
+
+      this.depStore['esn-config'] = function() {
+        return {
+          get: function(callback) {
+            return callback(null, {});
+          }
+        };
+      };
+
+      require('../../lib/module')(this.dependencies, function(err, lib) {
+        expect(err).to.not.exist;
+        lib.sendInvitation(from, to, {url: 'anURL'}, function(err) {
+          expect(err).to.exist;
+          expect(err.message).to.match(/Can not get a valid email sender/);
+          done();
+        });
       });
     });
 
@@ -68,13 +120,12 @@ describe('The emailInvitation lib', function() {
       });
     });
 
-
     it('should call mailer sendMethod', function(done) {
       var from = {objectType: 'user', id: 'robert@pipo.com'};
       var to = {objectType: 'user', id: 'gerard@pipo.com'};
 
       this.depStore.mailer.sendHTML = function(fromAddr, toAddr, subject, template, locales, callback) {
-        expect(fromAddr).to.equal(from.id);
+        expect(fromAddr).to.equal(mail.mail.noreply);
         expect(toAddr).to.equal(to.id);
         return callback();
       };
