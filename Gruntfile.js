1335c31285a037550d985f5c498c16d54b4e18e0
Michael Bailly <mbailly@linagora.com>
2015-11-02 16:55:44 +0100

    MEET-496 provide webserver routes to send client js and images

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
       options: {
         flags: [
           '--disable 0110',
-          '--jsdoc',
+          '--nojsdoc',
           '-e test/frontend/karma-include'
         ],
         reporter: {
